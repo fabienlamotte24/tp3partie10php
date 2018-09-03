92d6b10e75ac865c3b03a3f5b1f54c5b65090714
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-09-03 11:45:25 +0200

    exercice terminé

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php 
+    $portrait1 = array('name'=>'Victor', 'firstname'=>'Hugo', 'portrait'=>'http://upload.wikimedia.org/wikipedia/commons/5/5a/Bonnat_Hugo001z.jpg');
+    $portrait2 = array('name'=>'Jean', 'firstname'=>'de La Fontaine', 'portrait'=>'http://upload.wikimedia.org/wikipedia/commons/e/e1/La_Fontaine_par_Rigaud.jpg');
+    $portrait3 = array('name'=>'Pierre', 'firstname'=>'Corneille', 'portrait'=>'http://upload.wikimedia.org/wikipedia/commons/2/2a/Pierre_Corneille_2.jpg');
+    $portrait4 = array('name'=>'Jean', 'firstname'=>'Racine', 'portrait'=>'http://upload.wikimedia.org/wikipedia/commons/d/d5/Jean_racine.jpg');
+    portrait($portrait1);
+    portrait($portrait2);
+    portrait($portrait3);
+    portrait($portrait4);
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <title>TP 3</title>
+  </head>
+  <body>
+      <div class="container-fluid">
+          <div class="row">
+              <div class="board offset-3 col-6">
+                  <?php //la fonction showArray($array) se répète pour chaque tableau $portrait
+            function portrait($array) {?>
+                <!--Affiche l'element de la clé name-->
+                <p class="text"><?php echo $array['name'] . ' ' . $array['firstname'] . ' ' ; ?></p>
+                <p><img class="picture" src=" <?= $array['portrait'] ; ?>" /></p>
+                <?php  } ?>
+              </div>
+          </div>
+      </div>
+  </body>
+</html>
